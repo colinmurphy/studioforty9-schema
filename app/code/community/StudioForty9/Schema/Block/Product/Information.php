72476b95d1dd1dc46db828abd59af3f0f1a32b84
Colin Murphy <colin@colinfmurphy.com>
2015-09-25 14:38:28 +0100

    Refactored information block

--- app/code/community/StudioForty9/Schema/Block/Product/Information.php
+++ app/code/community/StudioForty9/Schema/Block/Product/Information.php
@@ -10,31 +10,65 @@
  * @author    Colin Murphy <colin@studioforty9.com>
  * @copyright 2015 StudioForty9
  */
-class StudioForty9_Schema_Block_Product_Information
-    extends Mage_Core_Block_Template
+class StudioForty9_Schema_Block_Product_Information extends Mage_Core_Block_Template
 {
     /**
      * Gets the image file
      *
      * @param Mage_Catalog_Model_Product $_product
      *
-     * @return bool|string
+     * @return string
      */
-    public function getImageFile(Mage_Catalog_Model_Product $_product)
+    public function getImageFile(Mage_Catalog_Model_Product $product)
     {
-        $_image = $_product->getImage();
-        if (! $_image || $_image == 'no_selection')
-        {
-            $_image = $_product->getMediaGalleryImages()->getFirstItem();
-            if (! $_image) {
-                return false;
-            }
-            $_image = $_image->getFile();
-            if (! $_image) {
-                return false;
-            }
+        $image = $this->_getImageFromProduct($product);
+        $image = (!$image) ? $this->_getImageFromGallery($product) : $image;
+        $image = trim($image);
+        if (!$image) {
+            return '';
         }
-        return Mage::getBaseUrl(Mage_Core_Model_Store::URL_TYPE_MEDIA)
-        . 'catalog/product' . $_image;
+
+        $url = Mage::getBaseUrl(Mage_Core_Model_Store::URL_TYPE_MEDIA);
+        return $url . 'catalog/product' . $image;
     }
+
+    /**
+     * Get the image from the product.
+     *
+     * @param  Mage_Catalog_Model_Product $product
+     *
+     * @return string
+     */
+    private function _getImageFromProduct(Mage_Catalog_Model_Product $product)
+    {
+        $image = $product->getImage();
+        if (!$image || $image === 'no_selection') {
+            return '';
+        }
+
+        return $image;
+    }
+
+    /**
+     * Get the first image from the image gallery.
+     *
+     * @param  Mage_Catalog_Model_Product $product
+     *
+     * @return string
+     */
+    private function _getImageFromGallery(Mage_Catalog_Model_Product $product)
+    {
+        $gallery = $product->getMediaGalleryImages();
+        if (!$gallery->count()) {
+            return '';
+        }
+
+        $image = $gallery->getFirstItem();
+        if (!$image || !$image->hasFile()) {
+            return '';
+        }
+
+        return $image->getFile();
+    }
+
 }
